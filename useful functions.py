454f60d6fc3ff2af812a55887bf9698d36589db8
Guillaume SCHERRER <114092655+guiguiSCH68@users.noreply.github.com>
2023-05-10 09:44:31 +0200

    Update useful functions.py

--- useful functions.py
+++ useful functions.py
@@ -1,17 +1,11 @@
-def Tab_diff_div(X,Y):
-    if(len(X)==1):
-        return Y[0]
-    else:
-        nom = Tab_diff_div(X[1:len(X)], Y[1:len(Y)]) - Tab_diff_div(X[0:len(X)-1], Y[0:len(Y)-1])
-        den = (X[-1]-X[0])
-        return nom/den
+interpolation_initiale = interpol_process(liste_support_initial, liste_valeur_initial)
 
-def EvalH(C,A,t):
-  
-    n=len(A)
-    valeur=A[n-1] # on affecte la dernière valeur de A
-    for i in range(n-2,-1,-1): # on part du rang n-2 ( = av dernière valeur) on s'arrête au range -1 (0 du coup) par pas de -1 
-        valeur=valeur*(t-C[i])+A[i]
-    return valeur
-  
- 
+#template for plotting an interpolation
+
+plt.plot(interpolation_initiale[1],interpolation_initiale[0], label = 'interrpolation')
+plt.plot(liste_support_initial,liste_valeur_initial,label = "points du support")
+plt.xlabel('x')
+plt.ylabel(' y')
+plt.title('Interpolation')
+plt.grid()
+plt.show()
